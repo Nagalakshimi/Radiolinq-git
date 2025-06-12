907c5640425bb1766106d069b43e44c214727d0c
Nagalakshimi <nagalakshimi@radiolinq.com>
2025-06-12 16:00:28 +0530

    code added for filtering the name of the patient

--- tests/filters/patient_name.spec.js
+++ tests/filters/patient_name.spec.js
@@ -41,21 +41,61 @@ test('case filter',async()=>{
      await Patient_namefilter.click();
      await expect(Patient_namefilter).toBeEditable();
 
+     //Fill the text inside the patient_name textbox
+     await Patient_namefilter.fill('S');
+     await page.waitForTimeout(4000);
+
+     //Check the Patient details table is visible
+      const patient_detail_table = page.locator('.ant-table-container');
+      await expect(patient_detail_table).toBeVisible();
+
+     //Verify to have patient_name inside the table
+      const patient_nameintable = await page.locator('//thead[@class="ant-table-thead"]//th[3]//div[contains(text(),"Patient Name")]');
+      await expect(patient_nameintable).toBeVisible();
+
+      //Locate all the table row and make it count
+                                    
+       const row_count = page.locator('//div[@class="ant-table-body"]//tr[@class="ant-table-row ant-table-row-level-0 cursor-pointer"]');
+       const total_row_count = await row_count.count();
+       console.log("Row count = "+total_row_count);
+
+       //Locate the 3rd column(patientname) in each row
+       for(let i=0; i<total_row_count; i++)
+       {
+         const patientname_cell = row_count.nth(i).locator('td').nth(2);
+         await expect(patientname_cell).toBeVisible();
+         //get the text
+             const patientsname = await patientname_cell.textContent();
+              if (!patientsname || patientsname.trim() === '') 
+                {
+                continue; // skip empty/fake rows
+                }
+                if (patientsname.toLowerCase().includes('s')) {
+                console.log("Row "+(i + 1) + "has name with 's': "+patientsname);
+                    }
+       }
+       
+
+     /*
      //Click the 9th filter - from date and choose the date
       const fromdate = await (filter_option.nth(8)).locator('//input[@name="fromDate"]');
       await fromdate.click();
+      await page.waitForSelector('.ant-picker-panel-container', {state: 'visible'});
       const monthlocator = page.locator('//div[@class="ant-picker-date-panel"]//div[@class="ant-picker-header-view"]//button[contains(text(),"Jun")]');
       await expect(monthlocator).toBeVisible();
+      
 
       //Click previous button to go to may month
-       const choosemonth = await page.locator('.ant-picker-header-prev-btn');
+       const choosemonth = await page.locator('button.ant-picker-header-prev-btn');
+       await expect(choosemonth).toBeVisible();
+       await page.waitForTimeout(1000);
        await choosemonth.click();
        const monthlocator1 = page.locator('//div[@class="ant-picker-date-panel"]//div[@class="ant-picker-header-view"]//button[contains(text(),"May")]');
        await expect(monthlocator1).toBeVisible();
 
        //Choose the date(16-may-2025)
         await page.locator('//div[@class="ant-picker-body"]//tr//td//div[contains(text(),"16")]').click();
-
+    */
 
 });
 
